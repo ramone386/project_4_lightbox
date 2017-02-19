8b6d76c3cdb44d6cfcb834c5beebe7558ebc45b1
Ramone Adams <radams386@gmail.com>
2017-02-19 06:01:30 -0600

    Project_4_lightbox

---
+++ js/navigate.js
@@ -0,0 +1,31 @@
+document.getElementById('links').onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+        /*global blueimp*/
+    blueimp.Gallery(links, options);
+    
+
+};
+
+
+// function myFunction() {
+//     // Declare variables
+//     var input, filter, ul, li, a, i;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("links");
+//     li = ul.getElementsByTagName('li');
+
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByTagName("a")[0];
+//         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
